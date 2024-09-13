07c621dca368d42bae54bede4793c96aa291623a
Joao Svaisser <svaisserjv@gmail.com>
2024-09-13 10:12:17 -0300

    fix  avatars

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function toogleMode(){
     const img = document.querySelector("#profile img")
 
     if(html.classList.contains('light')){
-        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute('src', './assets/avatar-light.jpg')
     } else{
-        img.setAttribute('src', './assets/avatar.png')
+        img.setAttribute('src', './assets/avatar.jpg')
     }
 }
